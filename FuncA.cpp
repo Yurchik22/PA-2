145e58f78d541231ca78d7c4c2a80247bc827239
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2024-10-24 22:21:13 +0300

    Added comments

--- FuncA.cpp
+++ FuncA.cpp
@@ -4,11 +4,11 @@
 FuncA::FuncA(){
 }
 
-double FuncA::Calculate(double x, int terms){
+double FuncA::Calculate(double x, int terms){ 
  double sum = 0.0;
     for (int n = 0; n < terms; ++n) {
-        double numerator = pow(-1, n) * tgamma(2 * n + 1);
-        double denominator = (1 - 2 * n) * pow(4, n) * pow(x, n);
+        double numerator = pow(-1, n) * tgamma(2 * n + 1); // (2n)! = Gamma(2n+1)
+        double denominator = (1 - 2 * n) * pow(4, n) * pow(x, n); 
         sum += numerator / denominator;
     }
     return sum;
